3ef69ad1277ffdebe3c5c6452ebefa27566a28a9
Vlascitchii-Mihail <mishavlascitchii@gmail.com>
2022-11-13 10:02:15 +0200

    Bits and Pizzas

--- Android/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/FragmentOrder.kt
+++ Android/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/FragmentOrder.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.hfad.bitsandpizzas.databinding.FragmentOrderBinding
 
 /**
@@ -25,12 +28,28 @@ class FragmentOrder : Fragment() {
             //setSupportActionBar(toolbar) in the fragment
             (activity as AppCompatActivity).setSupportActionBar(toolbar)
 
-            val pizzaType = pizzaGroup.checkedRadioButtonId
-            if (pizzaType == -1) {
-                //no item
-            } else {
-                //do something
+            fab.setOnClickListener {
+                val pizzaType = pizzaGroup.checkedRadioButtonId
+                if (pizzaType == -1) {
+                    Toast.makeText(activity, "The pizza hasn't been checked", Toast.LENGTH_SHORT).show()
+                } else {
+                    var text = when (pizzaType) {
+                        R.id.radio_diavolo -> "Diavolo pizza"
+                        else -> "Funghi pizza"
+                    } + if (parmesan.isChecked) ", extra parmesan" else ""
+                    text += if (chiliOil.isChecked) ", extra chilli oil" else ""
+
+                    Snackbar.make(fab, text, Snackbar.LENGTH_SHORT)
+
+                            //make action if the snackbar was clicked
+                        .setAction("Undo") {
+                            Toast.makeText(activity, "The pizza hasn't been checked", Toast.LENGTH_SHORT).show()
+
+                            //change the place of the snackbar
+                        }.show()
+                }
             }
+
         }
 
         return binding.root
